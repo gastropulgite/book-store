03eff41ade46ed1272bf217be25459b2ca0d6409
Hieu Nguyen <hieungming@gmail.com>
2022-06-15 09:34:34 +0700

    update helpers function

--- server/cmd/api/handlers.go
+++ server/cmd/api/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -19,27 +18,42 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	
 	var creds cridentials
 	var payload jsonResponse
-	err := json.NewDecoder(r.Body).Decode(&creds)
+
+	err := app.readJSON(w, r, &creds) 
 
 	if err != nil {
-		app.errorLog.Println("Invalid json!")
-
+		app.errorLog.Println(err)
 		payload.Error = true
-		payload.Message = "Invalid json!"
-		out, err := json.MarshalIndent(payload, "", "\t")
+		payload.Message = "Invalid JSON"
+		app.writeJSON(w, http.StatusBadRequest, payload)
+	}
+	// err := json.NewDecoder(r.Body).Decode(&creds)
+
+	// if err != nil {
+	// 	app.errorLog.Println("Invalid json!")
+
+	// 	payload.Error = true
+	// 	payload.Message = "Invalid json!"
+	// 	out, err := json.MarshalIndent(payload, "", "\t")
 		
-		if err != nil {
-			app.errorLog.Println(err)
-		}
+	// 	if err != nil {
+	// 		app.errorLog.Println(err)
+	// 	}
 		
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(out)
-		return
-	} 
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	w.WriteHeader(http.StatusOK)
+	// 	w.Write(out)
+	// 	return
+	// } 
 
 	app.infoLog.Println(creds.UserName, creds.Password)
 
 	payload.Error = false
 	payload.Message = "Signed In!"
+
+	err = app.writeJSON(w, http.StatusOK, payload)
+
+	if err != nil {
+		app.errorLog.Println(err)
+	}
 } 
